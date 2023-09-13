dddbc82874b76a28d3162b89f6395498364bd489
Alejandro Jaegerman <84936393+a-jaegerman@users.noreply.github.com>
2023-09-13 14:52:14 -0700

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,12 @@
+// src/components/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Auth from './Auth';
+import ChatBot from './ChatBot';
+import FinancialData from './FinancialData';
 
+const App = () => {
+  // ... code for routing and combining components
+};
+
+export default App;
